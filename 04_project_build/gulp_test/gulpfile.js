3eb30677d4c3fa77bd5e607fe79f09fc6a611656
xiongjian <webjsforyou@gmail.com>
2018-09-07 16:21:46 +0800

    gulp的js、css、html文件的处理

--- 04_project_build/gulp_test/gulpfile.js
+++ 04_project_build/gulp_test/gulpfile.js
@@ -2,6 +2,15 @@
 const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions', "not ie <= 8"] });
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const htmlmin = require('gulp-htmlmin');
 
 //2. 配置插件任务
 /*
@@ -27,6 +36,50 @@ gulp.task('babel', ['jshint'], () => {
     .pipe(gulp.dest('build/js'))   //将流中的文件输出出去
 })
 
+gulp.task('browserify', ['babel'], () => {
+  return gulp.src('build/js/main.js')
+    .pipe(browserify())   //将commonjs模块化语法转换浏览器能识别的语法
+    .pipe(rename('app.js'))   //重命名内存中/流中文件
+    .pipe(gulp.dest('./build/js'))
+})
+
+gulp.task('uglify', ['browserify'], () => {
+  return gulp.src('build/js/app.js')
+    .pipe(uglify())   //压缩js代码
+    .pipe(rename('dist.min.js'))   //重命名内存中/流中文件
+    .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('less', () => {
+  return gulp.src('./src/less/*.less')
+    .pipe(less({  //将less文件转换成css文件，并且扩展了css前缀
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest('./build/css'));
+})
+
+gulp.task('concat', ['less'], () => {
+  return gulp.src('./build/css/*.css')
+    .pipe(concat('built.css'))  //合并指定文件，命名合并后的文件
+    .pipe(gulp.dest('./build/css'));
+})
+
+gulp.task('cssmin', ['concat'], () => {
+  return gulp.src('./build/css/built.css')
+    .pipe(cssmin())  //压缩指定css文件
+    .pipe(rename('dist.min.css'))
+    .pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('htmlmin', () => {
+  return gulp.src('./src/index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))  //压缩指定html文件
+    .pipe(gulp.dest('./dist'));
+})
+
 
 //3. 配置默认任务
-gulp.task('default', ['babel']);
+gulp.task('default', ['uglify', 'cssmin', 'htmlmin']);
