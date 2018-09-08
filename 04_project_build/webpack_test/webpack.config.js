d976395b2be5843600d4d8d68046f4959e0a92b0
xiongjian <webjsforyou@gmail.com>
2018-09-08 11:56:14 +0800

    webpack的js和html文件的处理

--- 04_project_build/webpack_test/webpack.config.js
+++ 04_project_build/webpack_test/webpack.config.js
@@ -4,6 +4,8 @@
 const {resolve} = require('path');
 //引入插件模块
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   //入口
@@ -46,11 +48,57 @@ module.exports = {
             }
           }
         ]
+      },
+      {   //jshint  jshint-loader
+        test: /\.js$/, // 涵盖 .js 文件
+        enforce: "pre", // 预先加载好 jshint loader
+        exclude: /node_modules/, // 排除掉 node_modules 文件夹下的所有文件
+        include: [resolve(__dirname, 'src/js')],   //指定检查文件路径
+        use: [
+          {
+            loader: "jshint-loader",
+            options: {
+              //jslint 的错误信息在默认情况下会显示为 warning（警告）类信息
+              //将 emitErrors 参数设置为 true 可使错误显示为 error（错误）类信息
+              emitErrors: true,
+    
+              //jshint 默认情况下不会打断webpack编译
+              //如果你想在 jshint 出现错误时，立刻停止编译
+              //请设置 failOnHint 参数为true
+              failOnHint: true,
+              esversion: 6   //声明使用es6语法
+            }
+          }
+        ]
+      },
+      /*{   // babel-loader@8.0.0-beta.0 @babel/core @babel/preset-env
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }*/
+      {   // babel-preset-es2015  babel-loader@7 babel-core
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
       }
     ]
   },
   //插件
   plugins: [
-    new ExtractTextPlugin("./css/styles.css"),
+    new ExtractTextPlugin("./css/index.css"),
+    new HtmlWebpackPlugin({   //以指定文件为模板创建新的html文件，新的文件内会自动引入打包生成的css和js
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(['build'])  //清除指定目录下所有的文件
   ]
 }
