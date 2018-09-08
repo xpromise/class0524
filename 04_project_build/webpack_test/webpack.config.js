c84c2be14422841e226218399f21499fe982b728
xiongjian <webjsforyou@gmail.com>
2018-09-08 10:51:05 +0800

    webpack的图片处理

--- 04_project_build/webpack_test/webpack.config.js
+++ 04_project_build/webpack_test/webpack.config.js
@@ -2,6 +2,8 @@
   webpack的默认配置文件  webpack.config.js
  */
 const {resolve} = require('path');
+//引入插件模块
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   //入口
@@ -14,7 +16,7 @@ module.exports = {
   //loader
   module: {
     rules: [  //放置loader规则
-      {
+     /* {   //less less-loader css-loader style-loader
         test: /\.less$/,  //当前loader要处理的文件
         use: [{         //一旦遇见了指定文件，就执行use中的loader处理此文件   执行顺序都是从右到左
           loader: "style-loader" // 将js字符串的css代码，最终生成一个style标签插入到页面中
@@ -23,7 +25,32 @@ module.exports = {
         }, {
           loader: "less-loader" // 将less编译成css
         }]
+      },*/
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "less-loader"]
+        })
+      },
+      {   //file-loader url-loader
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,    //将图片大小为8kb以下的做base64处理，图片资源的优化
+              publicPath: '../images',  //决定src/url属性文件的路径
+              outputPath: './images',   //决定文件输出的路径
+              name: '[hash:7].[ext]'   //重命名图片文件，hash文件名， ext文件扩展名
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  //插件
+  plugins: [
+    new ExtractTextPlugin("./css/styles.css"),
+  ]
 }
